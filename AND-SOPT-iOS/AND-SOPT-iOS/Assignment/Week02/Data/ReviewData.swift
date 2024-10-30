9ce044e2d7d431ddf900a1dca547826040658ac7
sem-git <lsmsm0327@naver.com>
2024-10-30 19:45:22 +0900

    refactor: 리뷰 데이터 분리 및 UILabel extension 추가로 레이블 생성 코드 간소화

---
+++ AND-SOPT-iOS/AND-SOPT-iOS/Assignment/Week02/Data/ReviewData.swift
@@ -0,0 +1,48 @@
+//
+//  ReviewData.swift
+//  AND-SOPT-iOS
+//
+//  Created by 이세민 on 10/30/24.
+//
+
+import Foundation
+
+struct ReviewData {
+    let reviewTitle: String
+    let reviewRating: String
+    let reviewDate: String
+    let reviewer: String
+    let reviewContent: String
+    let developer: String
+    let answerDate: String
+    let answerContent: String
+}
+
+extension ReviewData {
+    static let mockData: [ReviewData] = [
+        ReviewData(reviewTitle: "토스 UX 전버전으로 해주세요",
+                   reviewRating: "★★★★★",
+                   reviewDate: "9월 27일 •",
+                   reviewer: "흑 운영자님 ㅠㅠ",
+                   reviewContent: "최근 업데이트가 토스 만의 ux 색깔 개성자체를\n잃어버린 것 같습니다. 메인 화면 볼때마다 되게\n부드럽고 한눈에 보기 편했는데, 이번 업데이트로\n인해 딱딱해진 것 같네요. 새로움을 지향하는건\n좋으나 이용자들에게 강제가 아닌 선택할 수 있는\n옵션이라도 만들어 주셨으면 어떨까요?",
+                   developer: "개발자 답변",
+                   answerDate: "9월 29일",
+                   answerContent: "안녕하세요. 토스팀입니다. 소중한 의견을 주셔서\n너무나 감사합니다. 토스 화면 UI를 사용자의 \n요구를 반영해 조금 더 편리하게 사용하도록 변경\n 하였습니다만, 고객님처럼 불편하게 느끼셨을\n수도 있다고 생각합니다. 사용에 불편을 드려 죄송\n합니다.\n고객님께서 말씀해주신 내용은 반영될 수 있다\n확답 드리기는 어려우나, 팀내에 공유하여 보다\n편리한 토스 사용 경험을 하실 수 있도록 노력\n하겠습니다. 다른 문의 사항이 있다면 24시간 운영\n되는 카카오톡(@toss) 또는 고객센터 1599-\n4905로 문의 부탁드립니다. 감사합니다."),
+        ReviewData(reviewTitle: "굿굿",
+                   reviewRating: "★★★★★",
+                   reviewDate: "9월 20일 •",
+                   reviewer: "리뷰어1",
+                   reviewContent: "굿이에요",
+                   developer: "개발자 답변",
+                   answerDate: "9월 22일",
+                   answerContent: "안녕하세요. 토스팀입니다. 소중한 의견을 주셔서\n너무나 감사합니다. 토스 화면 UI를 사용자의 \n요구를 반영해 조금 더 편리하게 사용하도록 변경\n 하였습니다만, 고객님처럼 불편하게 느끼셨을\n수도 있다고 생각합니다. 사용에 불편을 드려 죄송\n합니다.\n고객님께서 말씀해주신 내용은 반영될 수 있다\n확답 드리기는 어려우나, 팀내에 공유하여 보다\n편리한 토스 사용 경험을 하실 수 있도록 노력\n하겠습니다. 다른 문의 사항이 있다면 24시간 운영\n되는 카카오톡(@toss) 또는 고객센터 1599-\n4905로 문의 부탁드립니다. 감사합니다."),
+        ReviewData(reviewTitle: "우하하",
+                   reviewRating: "★★★★★",
+                   reviewDate: "9월 24일 •",
+                   reviewer: "리뷰어2",
+                   reviewContent: "우웩",
+                   developer: "개발자 답변",
+                   answerDate: "9월 25일",
+                   answerContent: "안녕하세요. 토스팀입니다. 소중한 의견을 주셔서\n너무나 감사합니다. 토스 화면 UI를 사용자의 \n요구를 반영해 조금 더 편리하게 사용하도록 변경\n 하였습니다만, 고객님처럼 불편하게 느끼셨을\n수도 있다고 생각합니다. 사용에 불편을 드려 죄송\n합니다.\n고객님께서 말씀해주신 내용은 반영될 수 있다\n확답 드리기는 어려우나, 팀내에 공유하여 보다\n편리한 토스 사용 경험을 하실 수 있도록 노력\n하겠습니다. 다른 문의 사항이 있다면 24시간 운영\n되는 카카오톡(@toss) 또는 고객센터 1599-\n4905로 문의 부탁드립니다. 감사합니다."),
+    ]
+}
--- AND-SOPT-iOS/AND-SOPT-iOS/Assignment/Week02/Section/Review.swift
+++ AND-SOPT-iOS/AND-SOPT-iOS/Assignment/Week02/Section/Review.swift
@@ -56,7 +56,7 @@ class Review: UIView {
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.showsHorizontalScrollIndicator = false
-//        scrollView.isPagingEnabled = true
+        //        scrollView.isPagingEnabled = true
         return scrollView
     }()
     
@@ -104,8 +104,8 @@ class Review: UIView {
     }
     
     func setReviewContentViews() {
-        for _ in 0..<5 {
-            reviewStackView.addArrangedSubview(createReviewContentView())
+        for reviewData in ReviewData.mockData {
+            reviewStackView.addArrangedSubview(createReviewContentView(with: reviewData))
         }
     }
     
@@ -210,54 +210,21 @@ class Review: UIView {
         }
     }
     
-    private func createReviewContentView() -> UIView {
+    private func createReviewContentView(with reviewData: ReviewData) -> UIView {
         let reviewContentView = UIView()
         reviewContentView.backgroundColor = .tertiarySystemGroupedBackground
         reviewContentView.layer.cornerRadius = 15
         reviewContentView.clipsToBounds = true
         
-        let reviewTitleLabel = UILabel()
-        reviewTitleLabel.text = "토스 UX 전버전으로 해주세요"
-        reviewTitleLabel.font = .systemFont(ofSize: 17, weight: .heavy)
-        reviewTitleLabel.textColor = .white
+        let reviewTitleLabel = UILabel.createLabel(text: reviewData.reviewTitle, fontSize: 17, weight: .heavy)
+        let reviewRatingLabel = UILabel.createLabel(text: reviewData.reviewRating, fontSize: 13)
+        let reviewDateLabel = UILabel.createLabel(text: reviewData.reviewDate, fontSize: 15, color: .lightGray)
+        let reviewerLabel = UILabel.createLabel(text: reviewData.reviewer, fontSize: 15, color: .lightGray)
+        let reviewContentLabel = UILabel.createLabel(text: reviewData.reviewContent, fontSize: 15, color: .lightGray, lines: 2)
+        let developerLabel = UILabel.createLabel(text: reviewData.developer, fontSize: 15)
+        let answerDateLabel = UILabel.createLabel(text: reviewData.answerDate, fontSize: 15, color: .lightGray)
+        let answerContentLabel = UILabel.createLabel(text: reviewData.answerContent, fontSize: 15, color: .lightGray, lines: 2)
         
-        let reviewRatingLabel = UILabel()
-        reviewRatingLabel.text = "★★★★★"
-        reviewRatingLabel.font = .systemFont(ofSize: 13)
-        reviewRatingLabel.textColor = .white
-        
-        let reviewDateLabel = UILabel()
-        reviewDateLabel.text = "9월 27일 •"
-        reviewDateLabel.font = .systemFont(ofSize: 15)
-        reviewDateLabel.textColor = .lightGray
-        
-        let reviewerLabel = UILabel()
-        reviewerLabel.text = "흑 운영자님 ㅠㅠ"
-        reviewerLabel.font = .systemFont(ofSize: 15)
-        reviewerLabel.textColor = .lightGray
-        
-        let reviewContentLabel = UILabel()
-        reviewContentLabel.text = "최근 업데이트가 토스 만의 ux 색깔 개성자체를\n잃어버린 것 같습니다. 메인 화면 볼때마다 되게\n부드럽고 한눈에 보기 편했는데, 이번 업데이트로\n인해 딱딱해진 것 같네요. 새로움을 지향하는건\n좋으나 이용자들에게 강제가 아닌 선택할 수 있는\n옵션이라도 만들어 주셨으면 어떨까요?"
-        reviewContentLabel.font = .systemFont(ofSize: 15)
-        reviewContentLabel.numberOfLines = 2
-        reviewContentLabel.textColor = .lightGray
-        
-        let developerLabel = UILabel()
-        developerLabel.text = "개발자 답변"
-        developerLabel.font = .systemFont(ofSize: 15)
-        developerLabel.textColor = .white
-        
-        let answerDateLabel = UILabel()
-        answerDateLabel.text = "9월 29일"
-        answerDateLabel.font = .systemFont(ofSize: 15)
-        answerDateLabel.textColor = .lightGray
-        
-        let answerContentLabel = UILabel()
-        answerContentLabel.text = "안녕하세요. 토스팀입니다. 소중한 의견을 주셔서\n너무나 감사합니다. 토스 화면 UI를 사용자의 \n요구를 반영해 조금 더 편리하게 사용하도록 변경\n 하였습니다만, 고객님처럼 불편하게 느끼셨을\n수도 있다고 생각합니다. 사용에 불편을 드려 죄송\n합니다.\n고객님께서 말씀해주신 내용은 반영될 수 있다\n확답 드리기는 어려우나, 팀내에 공유하여 보다\n편리한 토스 사용 경험을 하실 수 있도록 노력\n하겠습니다. 다른 문의 사항이 있다면 24시간 운영\n되는 카카오톡(@toss) 또는 고객센터 1599-\n4905로 문의 부탁드립니다. 감사합니다."
-        answerContentLabel.numberOfLines = 2
-        answerContentLabel.font = .systemFont(ofSize: 15)
-        answerContentLabel.textColor = .lightGray
-       
         reviewContentView.addSubviews(reviewTitleLabel, reviewRatingLabel, reviewDateLabel, reviewerLabel, reviewContentLabel, developerLabel, answerDateLabel, answerContentLabel)
         
         reviewContentView.snp.makeConstraints{
@@ -308,6 +275,18 @@ class Review: UIView {
         return reviewContentView
     }
 }
+
+extension UILabel {
+    static func createLabel(text: String, fontSize: CGFloat, weight: UIFont.Weight = .regular, color: UIColor = .white, lines: Int = 1) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = .systemFont(ofSize: fontSize, weight: weight)
+        label.textColor = color
+        label.numberOfLines = lines
+        return label
+    }
+}
+
 
 extension UIButton {
     static func createButton(title: String, iconName: String) -> UIButton {
